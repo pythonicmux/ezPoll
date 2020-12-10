411fa22e733326b7a4b0d0f719e88b7536d42acc
pythonicmux <michaelli1234@gmail.com>
2020-12-09 17:57:21 -0800

    Wrote header and constructor/destructor for tcpListener

--- include/ezPoll.hpp
+++ include/ezPoll.hpp
@@ -1,6 +1,39 @@
 #include <sys/epoll.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdexcept>
+#include <unistd.h>
 
 namespace ezPoll {
-    static const int t = 2;
+
+// tcpListener listens and processes data from TCP connections on a specified port.
+class tcpListener {
+public:
+    // Construct a listener that will monitor a TCP connection on a port. Throws an exception if creating the listener fails for any reason.
+    tcpListener(int port);
+
+    // Close the tcpListener.
+    ~tcpListener();
+
+    // The listener sleeps until some data comes from any senders to the socket. Will automatically accept new connections. Upon receiving data from any sender, loop will call processData(int fd) with the file descriptor for any ready sender's data to let processData process the data. processData could potentially be called multiple times for multiple events/senders in one call to loop. Once it is done processing the ready data, loop returns to the user. Returns -1 if there is an error, or 0 if processData was applied successfully on the data. 
+    template <typename F>
+    int loop(F processData);
+
+    // The listener loops forever and continually process incoming data with processData. Throws an exception if there is any error.
+    template <typename F>
+    void serve(F processData);
+
+private:
+    static const int MAX_EVENTS = 10; 
+
+    // An array of events that epoll will add ready file descriptors with available data to.
+    struct epoll_event arrivedEvents[MAX_EVENTS];
+    // The file descriptor for the listening socket.
+    int listenerSocketFd;
+    // File descriptor for the epoll struct.
+    int epollFd;
+}; 
+
 
 } // namespace ezPoll
--- src/ezPoll.cpp
+++ src/ezPoll.cpp
@@ -1,1 +1,49 @@
 #include <ezPoll.hpp>
+
+namespace ezPoll {
+
+tcpListener::tcpListener(int port) {
+    // Attempt to create a socket.
+    if ((this->listenerSocketFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        throw std::runtime_error{"Creating socket failed"};
+    }
+
+    // Attempt to attach the socket to the port specified by the user.
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    // Note that this will bind the socket to all interfaces in addition to localhost. 
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = port;
+
+    if (bind(this->listenerSocketFd, (struct sockaddr*) &address, sizeof(address)) < 0) {
+        throw std::runtime_error{"Binding socket failed"};
+    }
+
+    // Start listening on this port, allowing for MAX_EVENTS pending connections to accept, i.e. the length of the epoll event array (the choice of length seems arbitrary though, as long as it's not just one client waiting).
+    if (listen(this->listenerSocketFd, tcpListener::MAX_EVENTS) < 0) { 
+        throw std::runtime_error{"Listening socket failed"};
+    }
+
+    // Create an epoll instance to process new connections and incoming data.
+    if ((this->epollFd = epoll_create1(0)) < 0) {
+        throw std::runtime_error{"Creating epoll structure failed"};
+    }
+
+    // Register the listening socket to epoll to monitor for new connections.
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = this->listenerSocketFd;
+    if (epoll_ctl(this->epollFd, EPOLL_CTL_ADD, this->listenerSocketFd, &ev) == -1) {
+        throw std::runtime_error{"Adding the listener socket to the epoll structure failed"};
+    }
+}
+
+tcpListener::~tcpListener() {
+        // Here, instead of throwing an exception we just disregard the error code for the syscalls in-case this destructor is called during stack unwinding while processing another exception. 
+    close(this->epollFd);
+    close(this->listenerSocketFd);
+}
+
+
+} // namespace ezPoll
+
